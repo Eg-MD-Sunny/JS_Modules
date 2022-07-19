8a6f5cc3737634d0feed2f08e68dd5b7d6a58c6d
Md Naimur Zaman Sunny <egmdsunny@gmail.com>
2022-07-19 21:59:59 +0600

    Javascript ES6 Modules

---
+++ libery.js
@@ -0,0 +1,42 @@
+// export { message, user, test };
+ 
+// let message = "This way is ot good";
+
+//====>Create Function
+// export function user(name){
+//     console.log(`Username: ${name}`)
+// }
+
+//====>Return Function Value
+// export { user };
+//  function user(name){
+//     return `Username: ${name}`;
+// }
+
+//====>Create class
+//  class test{
+//     constructor(){
+//         console.log('Constructor Method');
+//     }
+// }
+
+// export default function(){
+//     console.log("Hello");
+// }
+
+
+//Final Example
+export { user };
+
+ function user(){
+    return `Hello! This is serious Mistake.`;
+}
+
+
+
+
+
+
+
+
+
---
+++ main.js
@@ -0,0 +1,22 @@
+// import * as yahoo from './libery.js';
+//import { message, test, user as us } from './libery.js';
+
+//---Important Example-01
+// import { default as yahoo } from './libery.js';
+// yahoo();
+
+//---Important Example-02
+// import yahoo from './libery.js';
+// yahoo();
+
+// console.log(yahoo.message);
+// document.body.innerHTML = yahoo.message;
+
+// console.log(yahoo.user("Banta"));
+
+// let a = new yahoo.test();
+
+
+//Final Example
+import { user } from './bridge.js';
+console.log(user());
